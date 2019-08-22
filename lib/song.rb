c7bfcfc63c227f1dcc6c27ce7cfc030bffec9e0c
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-22 03:50:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,10 @@ class Song
 
   def self.new_from_filename(filename)
     split_filename = filename.chomp(".mp3").split(" - ")
+    song = Song.new 
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song
   end
 
 end
