513fec1068459fb0c6c69e59c31bd16669140cf8
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-22 02:47:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,46 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+  end
+
+  def self.find_by_name(name)
+    Song.all.detect{|song| song.name == name}
+  end
+
   def save
     self.class.all << self
   end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song
+      return song
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|alpha| alpha.name}
+  end
+
+  def self.new_from_filename(filename)
 
 end
